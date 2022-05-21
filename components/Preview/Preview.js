24501f1a8d6109447f43b2a094ed673dd51d34c2
donganh <spman510@gmail.com>
2022-05-22 01:47:11 +0700

    Fix copy to Clipboard

--- components/Preview/Preview.js
+++ components/Preview/Preview.js
@@ -1,100 +1,115 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useRef } from 'react'
-import ReactMarkdown from "react-markdown";
-import remarkMath from "remark-math";
-import remarkGFM from "remark-gfm";
-import rehypeRaw from "rehype-raw";
-import rehypeKatex from "rehype-katex";
+import ReactMarkdown from 'react-markdown'
+import remarkMath from 'remark-math'
+import remarkGFM from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import rehypeKatex from 'rehype-katex'
 import rehypeHighlight from 'rehype-highlight'
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Icon } from '@iconify/react';
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Icon } from '@iconify/react'
 import styles from './Preview.module.css'
-import slugify from 'slugify';
+import slugify from 'slugify'
+import Children from 'react-children-utilities'
 
-// const rehypeKatex = dynamic(() => import("rehype-katex").then((mod) => mod.default))
+const Preview = props => {
+	const { content, device, setTableOfContents, startTransition } = props
+	const preview = useRef()
 
-const Preview = (props) => {
+	const getSlug = hTag => {
+		return slugify(hTag.innerText, {
+			lower: true, // convert to lower case, defaults to `false`
+			strict: true // strip special characters except replacement, defaults to `false`
+		})
+	}
 
-    const { content, device, setTableOfContents, startTransition } = props;
-    const preview = useRef()
+	useEffect(() => {
+		if (device) {
+			startTransition(() => {
+				const hTags = preview.current.querySelectorAll('h1, h2, h3')
+				let headings = []
+				const scrollPosition = window.scrollY
+				hTags.forEach((hTag, index) => {
+					const tag = hTag.tagName
+					const title = hTag.innerText
+					const slug = getSlug(hTag) + `-${index}`
+					hTag.setAttribute('id', slug)
+					headings.push({
+						tag: tag,
+						title: title,
+						slug: slug,
+						top: hTag.getBoundingClientRect().top + scrollPosition
+					})
+				})
+				setTableOfContents(() => headings)
+			})
+		}
 
-    const getSlug = (hTag) => {
-        return slugify(hTag.innerText, {
-            lower: true,      // convert to lower case, defaults to `false`
-            strict: true     // strip special characters except replacement, defaults to `false`
-        })
-    }
+		return () => {}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [device])
 
-    useEffect(() => {
-        if (device) {
-            startTransition(() => {
-                const hTags = preview.current.querySelectorAll("h1, h2, h3");
-                let headings = []
-                const scrollPosition = window.scrollY
-                hTags.forEach((hTag, index) => {
-                    const tag = hTag.tagName
-                    const title = hTag.innerText
-                    const slug = getSlug(hTag) + `-${index}`
-                    hTag.setAttribute("id", slug)
-                    headings.push({
-                        tag: tag,
-                        title: title,
-                        slug: slug,
-                        top: hTag.getBoundingClientRect().top + scrollPosition
-                    })
-                })
-                setTableOfContents(() => headings)
-            })
-        }
-
-        return () => {
-
-        }
-    }, [device])
-
-    return (
-        <div className={styles.preview} ref={preview}>
-            <div className="markdown-body">
-                <ReactMarkdown
-                    {...props}
-                    children={content}
-                    remarkPlugins={[remarkGFM, remarkMath]}
-                    rehypePlugins={[rehypeRaw, rehypeKatex, [rehypeHighlight, { ignoreMissing: true, subset: true }]]}
-                    // rehypePlugins={[rehypeHighlight, rehypeKatex, rehypeRaw]}
-                    components={{
-                        code({ node, children, inline, className, ...props }) {
-                            if (!inline) {
-                                return (
-                                    <>
-                                        {children && (
-                                            <CopyToClipboard text={String(children).replace(/\n$/, '')}>
-                                                <div className={styles.clipboard} onClick={
-                                                    (e) => {
-                                                        e.target.classList.add(styles.tooltipped);
-                                                        setTimeout(() => {
-                                                            e.target.classList.remove(styles.tooltipped);
-                                                        }, 2000)
-                                                    }
-                                                }>
-                                                    <Icon icon="bx:bxs-copy" />
-                                                </div>
-                                            </CopyToClipboard>)}
-                                        <code className={className} {...props}>
-                                            {children}
-                                        </code>
-                                    </>
-                                )
-                            }
-                            return (
-                                <code className={className} {...props}>
-                                    {children}
-                                </code>
-                            )
-                        }
-                    }}
-                />
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.preview} ref={preview}>
+			<div className="markdown-body">
+				<ReactMarkdown
+					{...props}
+					remarkPlugins={[remarkGFM, remarkMath]}
+					rehypePlugins={[
+						rehypeRaw,
+						rehypeKatex,
+						[rehypeHighlight, { ignoreMissing: true, subset: true }]
+					]}
+					// rehypePlugins={[rehypeRaw, rehypeKatex]}
+					components={{
+						code({ node, children, inline, className, ...props }) {
+							if (!inline) {
+								console.log(Children.onlyText(children))
+								return (
+									<>
+										{children && (
+											<CopyToClipboard
+												text={Children.onlyText(
+													children
+												)}
+											>
+												<div
+													className={styles.clipboard}
+													onClick={e => {
+														e.target.classList.add(
+															styles.tooltipped
+														)
+														setTimeout(() => {
+															e.target.classList.remove(
+																styles.tooltipped
+															)
+														}, 2000)
+													}}
+												>
+													<Icon icon="bx:bxs-copy" />
+												</div>
+											</CopyToClipboard>
+										)}
+										<code className={className} {...props}>
+											{/* {parseReact(toHtml(tree))} */}
+											{children}
+										</code>
+									</>
+								)
+							}
+							return (
+								<code className={className} {...props}>
+									{children}
+								</code>
+							)
+						}
+					}}
+				>
+					{content}
+				</ReactMarkdown>
+			</div>
+		</div>
+	)
 }
 
 export default Preview
